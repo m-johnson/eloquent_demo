17847400cb59e3c43fe4ab4418417f4a7ff3b96c
Michael Johnson <michael.johnson@tylertech.com>
2024-02-28 16:14:13 -0600

    Init

---
+++ resources/views/jails.blade.php
@@ -0,0 +1,10 @@
+@extends('template')
+@section('content')
+    <table class="table">
+        <tr>
+            <th>ID</th>
+            <th>Jail Name</th>
+            <th>Date Created</th>
+        </tr>
+    </table>
+@endsection
